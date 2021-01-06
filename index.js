b17debbad5a14eab110802673647ba32943a1bb1
mngagan <mngagan@gmail.com>
2021-01-06 22:06:48 +0530

    4 files modified

--- index.js
+++ index.js
@@ -1,148 +1,205 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import './style.css';
-import config from './config.json'
-import _ from 'underscore'
-import { DialogComponent } from '@syncfusion/ej2-react-popups';
-import $ from 'jquery'
-import { QuestionAnswers } from './questionAnswers';
-
+import React, { Component } from "react";
+import { render } from "react-dom";
+import "./style.css";
+import config from "./config.json";
+import _ from "underscore";
+import { DialogComponent } from "@syncfusion/ej2-react-popups";
+import $ from "jquery";
+// import QuestionAnswers from "./quizQuestionsAnswers.json";
+// import quizQuestionsAnswers from "./quizQuestionsAnswers.json";
 
 // let effects = ['Zoom', 'FlipXDown', 'FlipXUp', 'FlipYLeft', 'FlipYRight']
-let effects = ['Zoom']
+let effects = ["Zoom"];
 class App extends Component {
-
   constructor() {
+    console.log("in constructoe", quizQuestionsAnswers);
     super();
     this.state = {
-      name: 'React',
+      name: "React",
       startPage: true,
       quiz: this.createCountObj(),
       showDialog: false,
-      activeId: null,
+      activeId: null
     };
     this.animationSettings = { effect: _.sample(effects), duration: 1000 };
-    this.dlgButton = [{
-      click: () => { this.dialogClose() },
-      buttonModel: { content: 'Done', isPrimary: true }
-    }];
-    this.seconds = 0
-    this.minutes = 0
+    this.dlgButton = [
+      {
+        click: () => {
+          this.dialogClose();
+        },
+        buttonModel: { content: "Done", isPrimary: true }
+      }
+    ];
+    this.seconds = 0;
+    this.minutes = 0;
   }
 
   componentDidMount = () => {
-    console.log('in did mount')
-    var that = this
-     $.ajax({url: "https://api.github.com/repos/mngagan/react-quiz-working/git/trees/master?recursive=1", success: function(result){
-    that.handleResponse({result})
-  }});
-    setInterval(function () {
-      if (that.seconds === 59) {
-        that.seconds = 0
-      }
-      else {
-        that.seconds = that.seconds + 1
-      }
-      $('#secs').html(that.seconds)
-    }, 1000);
+    console.log("in did mount");
+    var that = this;
+    // $.ajax({
+    //   url:
+    //     "https://api.github.com/repos/mngagan/react-quiz-working/git/trees/master?recursive=1",
+    //   success: function(result) {
+    //     that.handleResponse({ result });
+    //   }
+    // });
+    // setInterval(function() {
+    //   if (that.seconds === 59) {
+    //     that.seconds = 0;
+    //   } else {
+    //     that.seconds = that.seconds + 1;
+    //   }
+    //   $("#secs").html(that.seconds);
+    // }, 1000);
 
-    setInterval(function () {
-      if (that.minutes === 59) {
-        that.minutes = 0
-      }
-      else {
-        that.minutes = that.minutes + 1
-      }
-      $('#mins').html(that.minutes)
-    }, 60000);
-
-  }
-  handleResponse = (arg) => {
-    console.log('result', arg)
-  }
+    // setInterval(function() {
+    //   if (that.minutes === 59) {
+    //     that.minutes = 0;
+    //   } else {
+    //     that.minutes = that.minutes + 1;
+    //   }
+    //   $("#mins").html(that.minutes);
+    // }, 60000);
+  };
+  handleResponse = arg => {
+    console.log("result", arg);
+  };
   createCountObj = () => {
-    let count = config.count
-    let result = []
+    let count = config.count;
+    let result = [];
     _.range(1, count + 1).map((val, index) => {
       result.push({
         id: val,
-        active: true,
-      })
-    })
-    return result
-  }
+        active: true
+      });
+    });
+    return result;
+  };
 
   dialogClose() {
-    let quiz = this.state.quiz
+    let quiz = this.state.quiz;
     quiz = quiz.map((arg, index) => {
       if (arg.id === this.state.activeId) {
-        arg.active = false
+        arg.active = false;
       }
-      return arg
-    })
+      return arg;
+    });
     this.setState({ showDialog: false, quiz });
-    this.animationSettings.effect = _.sample(effects)
-
+    this.animationSettings.effect = _.sample(effects);
   }
   renderButtons = () => {
-    console.log('in render buttons')
+    console.log("in render buttons");
     return (
       <div style={{ textAlign: "center" }}>
         {this.state.quiz.map((val, index) => {
-          return <button key={_.random(99999) + ''} className='e-control e-btn e-outline e-primary buttonStyle' onClick={() => { this.handleButtonClick({ id: val.id }) }} id={val.id} disabled={!val.active}>{val.id}</button>
+          return (
+            <button
+              key={_.random(99999) + ""}
+              className="e-control e-btn e-outline e-primary buttonStyle"
+              onClick={() => {
+                this.handleButtonClick({ id: val.id });
+              }}
+              id={val.id}
+              disabled={!val.active}
+            >
+              {val.id}
+            </button>
+          );
         })}
       </div>
-    )
-  }
+    );
+  };
 
-  handleButtonClick = (arg) => {
-    let { id } = arg
-    console.log('open modal window');
+  handleButtonClick = arg => {
+    let { id } = arg;
+    console.log("open modal window");
     this.setState({
       activeId: id,
       showDialog: true
-    })
-  }
+    });
+  };
 
   render() {
-    let remainingCount = _.filter(this.state.quiz, (arg) => { return !arg.active }).length
-    window.state = this.state
+    let remainingCount = _.filter(this.state.quiz, arg => {
+      return !arg.active;
+    }).length;
+    window.state = this.state;
     return (
-      <div className='container'>
-        <div className='row'>
-          <div className='title'>
+      <div className="container">
+        <div className="row">
+          <div className="title">
             {config.title}
-            <div className={'remainingCount'}> {remainingCount} of {config.count} </div>
-            {false &&<div className={'time'}><span id='mins'>00</span> : <span id='secs'>0</span></div>}
+            <div className={"remainingCount"}>
+              {" "}
+              {remainingCount} of {config.count}{" "}
+            </div>
+            {false && (
+              <div className={"time"}>
+                <span id="mins">00</span> : <span id="secs">0</span>
+              </div>
+            )}
           </div>
         </div>
-        {config.type === 'image' && this.renderButtons()}
-        {this.state.showDialog && config.type === 'image' && this.renderModal()}
-        {config.type === 'questions' && <QuestionAnswers/>}
+        {(config.type === "image" || config.type === "questionsInModal") &&
+          this.renderButtons()}
+        {this.state.showDialog && config.type === "image" && this.renderModal()}
+        {config.type === "questions" && <QuestionAnswers />}
+        {this.state.showDialog &&
+          config.type === "questionsInModal" &&
+          this.renderQuestionsModal()}
       </div>
     );
   }
 
   renderModal = () => {
-    console.log('in render modal exectoino', this.state.showDialog);
-    return <DialogComponent
-      id='AnimationDialog'
-      isModal={true}
-      header={'Question ' + this.state.activeId}
-      showCloseIcon={true}
-      animationSettings={this.animationSettings}
-      width='80px'
-      height='80px'
-      ref={defaultDialog => this.defaultDialogInstance = defaultDialog}
-      target='#target'
-      buttons={this.dlgButton}
-      visible={this.state.showDialog}
-      beforeClose={() => this.dialogClose()}
-    >
-      {/* <span><img src='./images/1.png' width={500} height={450} className='imageStyle' /></span> */}
-      <img src={`./images/${this.state.activeId}.png`} width={500} height={450} alt = {'Not Found'} className='imageStyle' />
-    </DialogComponent>
-  }
+    return (
+      <DialogComponent
+        id="AnimationDialog"
+        isModal={true}
+        header={"Question " + this.state.activeId}
+        showCloseIcon={true}
+        animationSettings={this.animationSettings}
+        width="80px"
+        height="80px"
+        ref={defaultDialog => (this.defaultDialogInstance = defaultDialog)}
+        target="#target"
+        buttons={this.dlgButton}
+        visible={this.state.showDialog}
+        beforeClose={() => this.dialogClose()}
+      >
+        {/* <span><img src='./images/1.png' width={500} height={450} className='imageStyle' /></span> */}
+        <img
+          src={`./images/${this.state.activeId}.png`}
+          width={500}
+          height={450}
+          alt={"Not Found"}
+          className="imageStyle"
+        />
+      </DialogComponent>
+    );
+  };
+
+  renderQuestionsModal = () => {
+    return (
+      <DialogComponent
+        id="AnimationDialog"
+        isModal={true}
+        header={"Question " + this.state.activeId}
+        showCloseIcon={true}
+        animationSettings={this.animationSettings}
+        // width="80"
+        // height="80"
+        ref={defaultDialog => (this.defaultDialogInstance = defaultDialog)}
+        target="#target"
+        buttons={this.dlgButton}
+        visible={this.state.showDialog}
+        beforeClose={() => this.dialogClose()}
+      >
+        <div>{QuestionAnswers[0].question}</div>
+      </DialogComponent>
+    );
+  };
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
